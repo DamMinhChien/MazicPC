e04d5159c79e058e132a5c5f2fe44396fef8bc19
Đàm Minh Chiến <damminhchien220204@gmail.com>
2025-09-28 22:50:20 +0700

    Add 403 page

--- FE/MazicPC/src/pages/common/forbiddenPage/index.jsx
+++ FE/MazicPC/src/pages/common/forbiddenPage/index.jsx
@@ -1,5 +1,69 @@
+import { Link } from "react-router-dom";
+import styles from "./style.module.css";
+import ROUTERS from "../../../utils/router";
 const ForbiddenPage = () => {
-  return <div>ForbiddenPage</div>;
+  return (
+    <div className={styles.page}>
+      <div className={styles.mainContainer}>
+        {/* Bat 1 */}
+        <div className={styles.bat + " " + styles.fly1}>
+          <img
+            className={styles.wing + " " + styles.leftWing}
+            src="/403/bat-wing.png"
+            alt="Cánh trái"
+          />
+          <img className={styles.body} src="/403/bat-body.png" alt="Thân dơi" />
+          <img
+            className={styles.wing + " " + styles.rightWing}
+            src="/403/bat-wing.png"
+            alt="Cánh phải"
+          />
+        </div>
+
+        {/* Bat 2 */}
+        <div className={styles.bat + " " + styles.fly2}>
+          <img
+            className={styles.wing + " " + styles.leftWing}
+            src="/403/bat-wing.png"
+            alt="Cánh trái"
+          />
+          <img className={styles.body} src="/403/bat-body.png" alt="Thân dơi" />
+          <img
+            className={styles.wing + " " + styles.rightWing}
+            src="/403/bat-wing.png"
+            alt="Cánh phải"
+          />
+        </div>
+
+        {/* Bat 3 */}
+        <div className={styles.bat + " " + styles.fly3}>
+          <img
+            className={styles.wing + " " + styles.leftWing}
+            src="/403/bat-wing.png"
+            alt="Cánh trái"
+          />
+          <img className={styles.body} src="/403/bat-body.png" alt="Thân dơi" />
+          <img
+            className={styles.wing + " " + styles.rightWing}
+            src="/403/bat-wing.png"
+            alt="Cánh phải"
+          />
+        </div>
+
+        {/* Foreground */}
+        <img
+          className={styles.foregroundImg}
+          src="/403/HauntedHouseForeground.png"
+          alt="Ngôi nhà ma ám"
+        />
+      </div>
+
+      <h1 className={styles.errorCode}>FORBIDDEN 403</h1>
+      <Link to={ROUTERS.USER.HOME} className={styles.errorText}>
+        Khu vực này bị cấm. Hãy quay lại ngay!
+      </Link>
+    </div>
+  );
 };
 
 export default ForbiddenPage;
